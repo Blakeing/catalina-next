e0f9973dce3368ea105df8c4ba53692b177ba2c6
Blake Ingenthron <blakeingenthron@gmail.com>
2020-09-07 23:09:25 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import Nav from "../components/nav";
-import Head from "../components/head";
 
 export default function IndexPage() {
   return (
     <>
-      <Head title="Services" />
       <div>
         <Nav />
         <div className="py-20">
-          <h1>Next.js + Tailwind CSS</h1>
+          <h1>Maybe this fixes it!</h1>
         </div>
       </div>
     </>
